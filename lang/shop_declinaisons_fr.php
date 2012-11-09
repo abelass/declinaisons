bfd8235e966fe2ca62a46a09389d5371d8697d4f
abelass <websolutions@mychacra.net>
2012-11-09 12:23:21 +0100

    fusionner les fiches de langues

--- lang/shop_declinaisons_fr.php
+++ lang/shop_declinaisons_fr.php
@@ -5,16 +5,44 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
  
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	// S
-	'shop_declinaisons_titre' => 'Déclinaisons Produit',
+    // A
+    'ajouter_lien_declinaison' => 'Ajouter cette déclinaison',
 
-	// C
-	'cfg_exemple' => 'Exemple',
-	'cfg_exemple_explication' => 'Explication de cet exemple',
-	'cfg_titre_parametrages' => 'Paramétrages',
+    // C
+    'cfg_titre_parametrages' => 'Paramétrages',
 
-	// T
-	'titre_page_configurer_shop_declinaisons' => 'Configuration Déclinaisons',
+    // I
+    'icone_creer_declinaison' => 'Créer une déclinaison',
+    'icone_modifier_declinaison' => 'Modifier cette déclinaison',
+    'info_1_declinaison' => 'Une déclinaison',
+    'info_aucun_declinaison' => 'Aucune déclinaison',
+    'info_declinaisons_auteur' => 'Les déclinaisons de cet auteur',
+    'info_nb_declinaisons' => '@nb@ déclinaisons',
+
+    // L
+    'label_descriptif' => 'descriptif',
+    'label_id_parent' => 'declinaison_parente',
+    'label_titre' => 'titre',
+
+    // R
+    'retirer_lien_declinaison' => 'Retirer cette déclinaison',
+    'retirer_tous_liens_declinaisons' => 'Retirer toutes les déclinaisons',
+    
+    // S
+    'shop_declinaisons_titre' => 'Déclinaisons Produit',
+
+    // T
+    'texte_ajouter_declinaison' => 'Ajouter une déclinaison',
+    'texte_changer_statut_declinaison' => 'Cette déclinaison est :',
+    'texte_creer_associer_declinaison' => 'Créer et associer une déclinaison',
+    'titre_declinaison' => 'Déclinaison',
+    'titre_declinaisons' => 'Déclinaisons',
+    'titre_declinaisons_rubrique' => 'Déclinaisons de la rubrique',
+    'titre_langue_declinaison' => 'Langue de cette déclinaison',
+    'titre_logo_declinaison' => 'Logo de cette déclinaison',
+    'titre_page_configurer_shop_declinaisons' => 'Configuration Déclinaisons',
+
+
 );
 
 ?>
