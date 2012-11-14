8d620fdbdea5e6d72f7341ade7bc1e0e22b6a8b6
abelass <websolutions@mychacra.net>
2012-11-14 13:28:52 +0100

    inetrevenir sur le formulaire prix

--- shop_declinaisons_pipelines.php
+++ shop_declinaisons_pipelines.php
@@ -8,12 +8,26 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 
-/*
- * Un fichier de pipelines permet de regrouper
- * les fonctions de branchement de votre plugin
- * sur des pipelines existants.
- */
+function shop_declinaisons_recuperer_fond($flux){
+    $fond=$flux['args']['fond'];
+    $contexte=$flux['args']['contexte'];
 
+    // inclure le champ déclinaison
+    if ($fond == 'formulaires/prix'){
+        include_spip('inc/config');
+        $afficher_prix =recuperer_fond('formulaires/inc-prix_affichage',$contexte);
+        $declinaison_champs=recuperer_fond('formulaires/inc-prix_champ',$contexte);
+        
+        $patterns = array('/<!--fini champs!-->/','/<div class="liste prix">(.*?)<\/div>/ims');
+        $replacements = array('<!--fini champs!-->'.$declinaison_champs,$afficher_prix);                        
+       $rep= preg_replace($patterns,$replacements,$flux['data']['texte'],1);
+        
+        $flux['data']['texte'] = $rep;
+        }
+        
+    
+    return $flux;
+}
 
 
 ?>
