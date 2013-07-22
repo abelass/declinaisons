79022837941d4a150c8881581573b2d0d2dcf253
Rainer Müller <rainer@websimple.be>
2013-07-22 16:19:36 +0200

    cr´er le champ id_declinaison

--- shop_declinaisons_administrations.php
+++ shop_declinaisons_administrations.php
@@ -18,27 +18,10 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 **/
 function shop_declinaisons_upgrade($nom_meta_base_version, $version_cible) {
 	$maj = array();
-	# quelques exemples
-	# (que vous pouvez supprimer !)
-	# 
-	# $maj['create'] = array(array('creer_base'));
-	#
-	# include_spip('inc/config')
-	# $maj['create'] = array(
-	#	array('maj_tables', array('spip_xx', 'spip_xx_liens')),
-	#	array('ecrire_config', array('shop_declinaisons', array('exemple' => "Texte de l'exemple")))
-	#);
-	#
-	# $maj['1.1.0']  = array(array('sql_alter','TABLE spip_xx RENAME TO spip_yy'));
-	# $maj['1.2.0']  = array(array('sql_alter','TABLE spip_xx DROP COLUMN id_auteur'));
-	# $maj['1.3.0']  = array(
-	#	array('sql_alter','TABLE spip_xx CHANGE numero numero int(11) default 0 NOT NULL'),
-	#	array('sql_alter','TABLE spip_xx CHANGE texte petit_texte mediumtext NOT NULL default \'\''),
-	# );
-	# ...
 
-	$maj['create'] = array(array('maj_tables', array('spip_declinaisons')));
 
+	$maj['create'] = array(array('maj_tables', array('spip_declinaisons','spip_prix_objets')));
+	$maj['1.0.2'] = array(array('maj_tables', array('spip_prix_objets')));
     
 	include_spip('base/upgrade');
 	maj_plugin($nom_meta_base_version, $version_cible, $maj);
